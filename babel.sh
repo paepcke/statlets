fe4cac99d2eb553e20d5b99f5b2f180aed425435
Andreas Paepcke <paepcke@cs.stanford.edu>
2016-09-08 16:04:49 -0700

    Major: re-organized statlets into their own subdirectories. Ported correlations to D3 V4, and ES6 import/export module scheme.

--- babel.sh
+++ babel.sh
@@ -1,19 +1,75 @@
 #!/bin/bash
 
-USAGE="Run Babel on everything below src, depositing the new files in ./lib"
+USAGE="./babel { confidence | correlation }. Run Babel on everything below src/html, depositing the new files in ./lib."
 
-if [[ $# == 1 ]]
+# Now go through all the options
+while getopts ":h" opt
+do
+    case $opt in
+        h)
+            shift
+            echo $USAGE
+            exit 0
+            ;;
+ 
+        \?)
+            echo $USAGE
+            break
+            exit 1
+            ;;
+    esac
+done
+
+# Make sure one arg is left after
+# the shifting above: the statlet
+# name to process
+
+if [ -z $1 ]
 then
-    echo $USAGE
-    exit 0
+  echo $USAGE
+  exit 1
 fi
 
-echo "Running Babel..."
-node_modules/babel-cli/bin/babel.js src -d lib
+babelEnv=$1
 
-echo "Copying HTML..."
-cp src/html/*.css src/html/*.html lib/html/
-cp src/html/js/*.min.* lib/html/js
+export BABEL_ENV=$babelEnv
 
-echo "Running Browserify to make package..."
-node_modules/.bin/browserify lib/html/js/confidenceEng.js -o lib/html/js/confidence.js
+case "${babelEnv}" in
+
+    confidence)
+
+        echo "Babelizing statlet ${babelEnv}..."
+        node_modules/babel-cli/bin/babel.js src/html/ -d lib/html/
+
+        echo "Copying HTML..."
+        cp src/html/confidence/*.css  lib/html/confidence
+        cp src/html/confidence/*.html lib/html/confidence
+        cp src/html/utils/*.css src/html/utils/*.html lib/html/utils
+        cp src/html/utils/js/*.min.* lib/html/utils/js
+
+        echo "Running Browserify to make package..."
+        node_modules/.bin/browserify lib/html/confidence/js/confidenceEng.js \
+                                     -o lib/html/confidence/js/confidence.js
+        ;;
+
+    correlation)
+
+        echo "Babelizing statlet ${babelEnv}..."
+        node_modules/babel-cli/bin/babel.js src/html/ -d lib/html/
+
+        echo "Copying HTML..."
+        cp src/html/correlation/*.css  lib/html/correlation
+        cp src/html/correlation/*.html lib/html/correlation
+        cp src/html/utils/*.css src/html/utils/*.html lib/html/utils
+        cp src/html/utils/js/*.min.* lib/html/utils/js
+        
+        echo "Running Browserify to make package..."
+        node_modules/.bin/browserify lib/html/correlation/js/correlationEng.js \
+                                     -o lib/html/correlation/js/correlation.js
+        ;;
+    *)
+        echo "Unknown statlet name '${babelEnv}'"
+        exit 1
+        ;;
+esac
+              
